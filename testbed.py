5b536d7297dc83c3252fce29551f43bc0e109557
ivan_gudkov <iagudkov@gmail.com>
2022-05-02 22:29:48 +0300

    Update testbed.py
    попытки сделать || алгоритм

--- testbed.py
+++ testbed.py
@@ -1,85 +1,177 @@
-"""def automatic_testing_RA():
-    G = nx.Graph()
-    raw_vert=[i for i in range(0,20)]
-    for i in range(10):
-        for_net=[]
-        raw_a=[]
-        raw_s=[]
-        raw_r=[]
-        raw_ra=[]
-        for i in range(20):
-            c= random.randint(0,19)
-            v=random.randint(0,19)
-            if(c==v):
-                while (c==v):
-                    c= random.randint(0,19)
-                    v=random.randint(0,19)
-                a=[c,v]
-                b= {c,v}
-                raw_ra.append(a)
-                raw_a.append(a)
-                raw_s.append(a)
-                raw_r.append(a)
-                
-                for_net.append(b)
-            else:
-                a=[c,v]
-                b= {c,v}
-                raw_ra.append(a)
-                raw_a.append(a)
-                raw_s.append(a)
-                raw_r.append(a)
-                
-                for_net.append(b)
-        G.add_edges_from(for_net)
+from dataclasses import dataclass, field
+import networkx as nx
+import numpy
+import collections
+from joblib import Parallel, delayed
+import math
+import time
 
-        etalon=list(nx.connected_components(G))
-        etalon=cleanup(etalon)
-        #clean_S=cleanup(S.Algorithm_S([s for s in range(0,20)], raw_s))
-        #print(clean_S)
-        #print("Alg_R")
-        #clean_R=cleanup(R.AlgorithmR([r for r in range(0,20)], raw_r))
-        #print(clean_R)
-        #print("Alg_A")
-        #clean_A=cleanup(A.Algorithm_A([a for a in range(0,20)], raw_a))
-        #print(clean_A)
-        clean_RA=cleanup(RA.AlgorithmRA([ra for ra in range(0,20)], raw_ra))
-        #print("Alg_RA")
-        #print(clean_RA)
-        
-        """assert compare(clean_R, etalon), 
-        assert compare(clean_A, etalon), 
-        assert compare(clean_S, etalon), 
-        assert compare(clean_RA, etalon), """
-        """if(not (compare(clean_R, etalon))):
-            print ("input")
-            print(((str(for_net)).replace("{","[")).replace("}","]"))
-            print("mistake alg R")
-            print(etalon)
-            print(clean_R)
-            #иногда сравнение ломается если порядок различается
-            #разберусь чуть позже
-            print("_______")
-        elif(not compare(clean_A, etalon)):
-            print ("input")
-            print(((str(for_net)).replace("{","[")).replace("}","]"))
-            print("mistake alg A")
-            print(etalon)
-            print(clean_A)
-            print("_______")
-        elif(not compare(clean_S, etalon)):
-            print ("input")
-            print(((str(for_net)).replace("{","[")).replace("}","]"))
-            print("mistake alg S")
-            print(etalon)
-            print(clean_S)
-            print("_______")"""
-        if(not compare(clean_RA, etalon)):
-            print ("input")
-            print(((str(for_net)).replace("{","[")).replace("}","]"))
-            print("mistake alg RA")
-            print(etalon)
-            print(clean_RA)
-            print("_______")
-        G.clear()
+# from multiprocessing import Pool
+import multiprocessing
+
+Vertex_raw = [0, 1, 2, 3, 4, 5, 6]
+Edges_raw = [[1, 2], [1, 3], [2, 3], [4, 3], [5, 6], [0, 4]]
+
+
+@dataclass
+class Vertex:
+    number: int = 0
+    parent: int = 0
+    old_parent: int = 0
+
+
+# попытка обойти ограничение на 1 параметр
+# костыли но у меня идеи заканчиваются
 """
+for each v do v.p = v;
+repeat
+for each arc (v, w) do if v.p < w.p then w.p ¬ v.p
+until no parent changes
+"""
+
+
+def findVertex(vert, Vertexes):
+    for i in Vertexes:
+        if i.number == vert:
+            return i
+
+
+def findVertex_v2(vert, Vertexes):
+    for i in Vertexes:
+        if type(i) == Vertex:
+            if i.number == vert:
+                return i
+    return None
+
+
+def findVertex_analog(vert, Vertexes):
+    for i in range(len(Vertexes)):
+        if (Vertexes[i]).number == vert:
+            return i
+
+
+def findVertex_analog_v2(vert, Vertexes):
+    for i in range(len(Vertexes)):
+        if type(i) == Vertex:
+            if (Vertexes[i]).number == vert:
+                return i
+    return None
+
+
+def prot_init_v3(element, sharedlist):
+    sharedlist.append(Vertex(element, element))
+
+
+def prot_naive_alg_v3(arc, sharedlist):
+    v = findVertex(arc[0], sharedlist)
+    w = findVertex(arc[1], sharedlist)
+    v_addr = findVertex_analog(arc[0], sharedlist)
+    w_addr = findVertex_analog(arc[1], sharedlist)
+    if v.parent < w.parent:
+        w.parent = v.parent
+        sharedlist[w_addr] = w
+        # print (v,w)
+
+
+# всё очень плохо
+def prot_naive_alg_v4(arc, sharedlist, sharedlist2):
+    if type(arc) != Vertex:
+        v = findVertex_v2(arc[0], sharedlist)
+        w = findVertex_v2(arc[1], sharedlist)
+        if v.parent < w.parent:
+            w.parent = v.parent
+        print(len(sharedlist2))
+        sharedlist2.append(v)
+        sharedlist2.append(w)
+        # sharedlist2.append(arc)
+
+
+def prot_naive_alg_v5(arc, sharedlist, sharedlist2):
+    if type(arc) != Vertex:
+        v = findVertex_v2(arc[0], sharedlist)
+        w = findVertex_v2(arc[1], sharedlist)
+        if v.parent < w.parent:
+            w.parent = v.parent
+        else:
+            v.parent = w.parent
+        write_confl_v = findVertex_v2(v.number, sharedlist2)
+        write_confl_v_addr = findVertex_analog_v2(v.number, sharedlist2)
+        write_confl_w = findVertex_v2(w.number, sharedlist2)
+        write_confl_w_addr = findVertex_analog_v2(w.number, sharedlist2)
+        if write_confl_v != None:
+            if write_confl_v.number > v.number:
+                sharedlist2[write_confl_v_addr] = v
+        else:
+            sharedlist2.append(v)
+        if write_confl_w != None:
+            if write_confl_w.number > w.number:
+                sharedlist2[write_confl_w_addr] = w
+        else:
+            sharedlist2.append(w)
+
+
+def prot_naive_alg_v6(arc, sharedlist):
+    v = findVertex_v2(arc[0], sharedlist)
+    w = findVertex_v2(arc[1], sharedlist)
+    write_confl_v = findVertex_v2(v.number, sharedlist)
+    write_confl_w = findVertex_v2(w.number, sharedlist)
+    write_confl_v_addr = findVertex_analog(v.number, sharedlist)
+    write_confl_w_addr = findVertex_analog(w.number, sharedlist)
+    if v.parent < w.parent:
+        w.parent = v.parent
+    if write_confl_v.parent > v.parent:
+        sharedlist[write_confl_v_addr] = v
+    if write_confl_w.parent > w.parent:
+        sharedlist[write_confl_w_addr] = w
+
+
+# вообще так и должно быть у них было write conflict resovled in favour of smaller value
+def step_cleanup_v1():
+    pass
+
+
+# по их идее процессы идут глобальными шагами поэтому я хочу  сделать что то похожее
+
+
+def step_forward_v1():
+    pass
+
+
+def test(x):
+    print(x)
+
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool()
+    manager = multiprocessing.Manager()
+    sharedlist = manager.list()
+    sharedlist2 = manager.list()
+    tasks = [(x, sharedlist) for x in Vertex_raw]
+    task2 = [(x, sharedlist) for x in Edges_raw]
+    pool.starmap(prot_init_v3, tasks)
+    pool.close()
+    pool2 = multiprocessing.Pool()
+    pool2.starmap(prot_naive_alg_v6, task2)
+    task2 = [(x, sharedlist) for x in Edges_raw]
+    pool2.starmap(prot_naive_alg_v6, task2)
+    task2 = [(x, sharedlist) for x in Edges_raw]
+    pool2.starmap(prot_naive_alg_v6, task2)
+    task2 = [(x, sharedlist) for x in Edges_raw]
+    pool2.starmap(prot_naive_alg_v6, task2)
+    task2 = [(x, sharedlist) for x in Edges_raw]
+    pool2.starmap(prot_naive_alg_v6, task2)
+    pool2.close()
+    print(sharedlist)
+    """pool=multiprocessing.Pool()
+    
+    pool3=multiprocessing.Pool()
+    manager=multiprocessing.Manager()
+    
+    
+    
+    #print(sharedlist)
+    
+    pool3.starmap(prot_naive_alg_v3, task2)
+    pool3.close()
+    """
+# наивный алгоритм (наверное)
